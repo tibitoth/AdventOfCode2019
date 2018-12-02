ab473adb21ad372b59d0b8ab8a5776dc089c6087
Tibor Tóth <totht91@gmail.com>
2018-12-02 22:39:16 +0100

    initial infrastructure and day 1 solver

---
+++ AdventOfCode2018/AdventOfCode2018.Infrastructure/IPuzzleSolver.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.IO;
+using System.Threading.Tasks;
+
+namespace AdventOfCode2018.Infrastructure
+{
+    public interface IPuzzleSolver
+    {
+        int Day { get; }
+
+        Task<string> SolveAsync(Stream input);
+    }
+}
