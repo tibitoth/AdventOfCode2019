57ea1b79f0f9ebf0330c48bcd1c111d0217565c7
Tibor Tóth <toth.tibor@aut.bme.hu>
2019-12-03 12:24:33 +0100

    day1 part1 fixes

--- AdventOfCode2019/AdventOfCode2019.Day1/PuzzleSolverFactory.cs
+++ AdventOfCode2019/AdventOfCode2019.Day1/PuzzleSolverFactory.cs
@@ -1,15 +1,33 @@
 ﻿using AdventOfCode2018.Infrastructure;
+using Microsoft.Extensions.DependencyInjection;
 using System;
 using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
 using System.Text;
 
 namespace AdventOfCode2019.Infrastructure
 {
     public class PuzzleSolverFactory : IPuzzleSolverFactory
     {
+        private readonly IServiceProvider _serviceProvider;
+
+        public PuzzleSolverFactory(IServiceProvider serviceProvider)
+        {
+            _serviceProvider = serviceProvider;
+        }
+
         public IPuzzleSolver Create(int day)
         {
-            throw new NotImplementedException();
+            var type = Assembly.GetExecutingAssembly()
+                .GetTypes()
+                .SingleOrDefault(t =>
+                {
+                    var attribute = t.GetCustomAttribute<DayAttribute>();
+                    return attribute != null && attribute.Day == day && typeof(IPuzzleSolver).IsAssignableFrom(t);
+                });
+
+            return (IPuzzleSolver)_serviceProvider.GetRequiredService(type);
         }
     }
 }
--- AdventOfCode2019/AdventOfCode2019.Runner/Program.cs
+++ AdventOfCode2019/AdventOfCode2019.Runner/Program.cs
@@ -1,6 +1,8 @@
 ﻿using AdventOfCode2018.Infrastructure;
 using AdventOfCode2019.Infrastructure;
+using AdventOfCode2019.Puzzles.Day1;
 using AdventOfCode2019.Runner;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using System;
@@ -17,9 +19,17 @@ namespace AdventOfCode2018.Day1
 
         private static IHostBuilder CreateHost(string[] args) =>
             Host.CreateDefaultBuilder(args)
+                .ConfigureAppConfiguration((ctx, builder) =>
+                {
+                    builder.AddUserSecrets<Program>();
+                })
                 .ConfigureServices((ctx, services) =>
                 {
                     var option = services.RegisterOption<AdventOfCodeOptions>(ctx.Configuration);
+                    services.AddTransient<PuzzleClientCookieHandler>();
+                    services.AddTransient<IPuzzleSolverFactory, PuzzleSolverFactory>();
+
+                    services.AddTransient<TheTyrannyOfTheRocketEquation>();
 
                     services.AddHttpClient<PuzzleClient>()
                         .ConfigureHttpClient(c => c.BaseAddress = new Uri(option.BaseUrl))
