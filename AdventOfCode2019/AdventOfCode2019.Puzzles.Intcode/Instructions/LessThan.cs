af75fdd03d8be11cd319b1c28a4489fa1e289ac5
Tibor Tóth <toth.tibor@aut.bme.hu>
2019-12-09 10:02:50 +0100

    intcode: equals, less than

---
+++ AdventOfCode2019/AdventOfCode2019.Puzzles.Intcode/Instructions/LessThan.cs
@@ -0,0 +1,20 @@
+﻿using AdventOfCode2019.Puzzles.Day2.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AdventOfCode2019.Puzzles.Intcode.Instructions
+{
+    public class LessThan : TwoOperandInstructionBase
+    {
+        public LessThan(Span<int> memory, int instructionAddress) 
+            : base(memory, instructionAddress)
+        {
+        }
+
+        protected override int ExecuteCore(int param1, int param2)
+        {
+            return param1 < param2 ? 1 : 0;
+        }
+    }
+}
