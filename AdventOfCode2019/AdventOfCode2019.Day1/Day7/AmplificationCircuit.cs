24e1882de6dbd5725a80c3df34f6e7840d16b12e
Tibor Tóth <toth.tibor@aut.bme.hu>
2019-12-13 13:59:03 +0100

    day 7 bugfixes and part 2 test and implementation

--- AdventOfCode2019/AdventOfCode2019.Day1/Day7/AmplificationCircuit.cs
+++ AdventOfCode2019/AdventOfCode2019.Day1/Day7/AmplificationCircuit.cs
@@ -9,6 +9,7 @@ using AdventOfCode2018.Infrastructure;
 using AdventOfCode2019.Infrastructure;
 using AdventOfCode2019.Puzzles.Extensions;
 using AdventOfCode2019.Puzzles.Intcode;
+using AdventOfCode2019.Puzzles.Intcode.Instructions.IO;
 using AdventOfCode2019.Puzzles.Tests.Extensions;
 
 namespace AdventOfCode2019.Puzzles.Day7
@@ -18,41 +19,49 @@ namespace AdventOfCode2019.Puzzles.Day7
     {
         public async Task<string> SolvePart1Async(Stream input)
         {
-            return (await GetMaxThrusterSignalAsync(input)).MaxSignal.ToString();
+            return (await GetMaxThrusterSignalAsync(input, PhaseSettingsType.OneShot)).MaxSignal.ToString();
         }
 
-        internal async Task<(int MaxSignal, int[] MaxPermutation)> GetMaxThrusterSignalAsync(Stream input)
+        internal async Task<(int MaxSignal, int[] MaxPermutation)> GetMaxThrusterSignalAsync(Stream input, PhaseSettingsType phaseSettingsType)
         {
             var line = await input.ReadLineAsync();
             int[] registers = line.Split(',').Select(x => int.Parse(x)).ToArray();
 
             var max = int.MinValue;
             int[] maxPermutation = null;
+            var phaseSettingsRange = phaseSettingsType switch
+            {
+                PhaseSettingsType.OneShot => Enumerable.Range(0, 5),
+                PhaseSettingsType.FeedbackLoop => Enumerable.Range(5, 5),
+            };
 
-            foreach (var p in Enumerable.Range(0,5).GetPermutations())
+            foreach (var p in phaseSettingsRange.GetPermutations())
             {
                 var permutation = p.ToArray();
-                var amplifiers = new IntcodeProgram[5];
-                for (int i = 0; i < amplifiers.Length; i++)
-                {
-                    amplifiers[i] = new IntcodeProgram(registers.ToArray()); //copy
-                }
-
-                var outputChannel = Channel.CreateUnbounded<int>();
-                await outputChannel.Writer.WriteAsync(0);
-
+                var amplifiers = new (IntcodeProgram Program, Channel<int> Input)[5];
                 for (int i = 0; i < amplifiers.Length; i++)
                 {
                     var inputChannel = Channel.CreateUnbounded<int>();
                     await inputChannel.Writer.WriteAsync(permutation[i]);
-                    await inputChannel.Writer.WriteAsync(await outputChannel.Reader.ReadAsync());
 
-                    outputChannel = Channel.CreateUnbounded<int>();
-
-                    await amplifiers[i].RunAsync(inputChannel, outputChannel);
+                    // copy registers
+                    amplifiers[i] = (new IntcodeProgram(registers.ToArray()), inputChannel);
                 }
 
-                var result = await outputChannel.Reader.ReadAsync();
+                await amplifiers[0].Input.Writer.WriteAsync(0);
+                var tasks = new Task[amplifiers.Length];
+
+                for (int i = 0; i < amplifiers.Length; i++)
+                {
+                    // wire up input and output channels
+                    // first amp's input is the last amp's output
+                    tasks[i] = amplifiers[i].Program.RunAsync(amplifiers[i].Input, amplifiers[(i + 1) % amplifiers.Length].Input);
+                }
+
+                await Task.WhenAll(tasks);
+
+                // Last amp's output is the first amp's input
+                var result = await amplifiers[0].Input.Reader.ReadAsync();
 
                 if (result > max)
                 {
@@ -64,9 +73,10 @@ namespace AdventOfCode2019.Puzzles.Day7
             return (max, maxPermutation);
         }
 
-        public Task<string> SolvePart2Async(Stream input)
+        public async Task<string> SolvePart2Async(Stream input)
         {
-            throw new NotImplementedException();
+            return (await GetMaxThrusterSignalAsync(input, PhaseSettingsType.FeedbackLoop)).MaxSignal.ToString();
+
         }
     }
 }
---
+++ AdventOfCode2019/AdventOfCode2019.Day1/Day7/PhaseSettingsType.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AdventOfCode2019.Puzzles.Day7
+{
+    public enum PhaseSettingsType
+    {
+        OneShot,
+        FeedbackLoop
+    }
+}
