df3c0e3e0011f70b574bc905c5abd7c4c7e0548a
Tibor Tóth <toth.tibor@aut.bme.hu>
2020-01-01 22:07:55 +0100

    part 2 init logic

--- AdventOfCode2019/AdventOfCode2019.Puzzles/Day13/CarePackage.cs
+++ AdventOfCode2019/AdventOfCode2019.Puzzles/Day13/CarePackage.cs
@@ -28,9 +28,22 @@ namespace AdventOfCode2019.Puzzles.Day13
         Ball = 4,
     }
 
+    internal enum BallDirection
+    {
+        TopLeft = 1,
+        TopRight = 2,
+        BottomLeft = 3, 
+        BottomRight = 4, 
+    }
+
     [Day(13)]
     public class CarePackage : IPuzzleSolver
     {
+        private Dictionary<(long x, long y), TileType> _tiles;
+        private (long x, long y) _ball;
+        private (long x, long y) _paddle;
+        private long _score;
+
         public async Task<string> SolvePart1Async(Stream input)
         {
             var line = await input.ReadLineAsync();
@@ -52,9 +65,65 @@ namespace AdventOfCode2019.Puzzles.Day13
                 .CountAsync(t => (TileType)t.Skip(2).Single() == TileType.Block);
         }
 
-        public Task<string> SolvePart2Async(Stream input)
+        private async Task CreateInitialStateAsync(Channel<long> output)
         {
-            throw new NotImplementedException();
+            _tiles = new Dictionary<(long x, long y), TileType>();
+            _ball = (0, 0);
+
+            await foreach (var tileData in output.Reader.ReadAllAsync().Batch(3))
+            {
+                var data = tileData.ToList();
+                var coord = (x: data[0], y: data[1]);
+                if (coord.x == -1 && coord.y == 0)
+                {
+                    _score = data[2];
+                }
+                else
+                {
+                    var type = (TileType) data[2];
+                    _tiles[coord] = type;
+
+                    if (type == TileType.Ball)
+                    {
+                        _ball = coord;
+                    }
+
+                    if (type == TileType.Paddle)
+                    {
+                        _paddle = coord;
+                    }
+                }
+            }
+        }
+
+        public async Task<string> SolvePart2Async(Stream input)
+        {
+            var line = await input.ReadLineAsync();
+            var registers = line.Split(',').Select(x => long.Parse(x)).ToArray();
+
+            // free coins, play mode
+            registers[0] = 2;
+
+            var program = new IntcodeProgram(registers);
+
+            var output = Channel.CreateUnbounded<long>();
+            var inputChannel = Channel.CreateBounded<long>(1);
+
+
+            var programTask = program.RunAsync(inputChannel, output);
+            var inputTask = Task.Run(async () =>
+            {
+                while (!programTask.IsCompleted)
+                {
+                    await inputChannel.Writer.WriteAsync(0);
+                }
+            });
+
+            await CreateInitialStateAsync(output);
+
+            await programTask;
+
+            return "";
         }
     }
 }
