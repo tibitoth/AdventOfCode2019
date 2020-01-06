2f3e4186eba3752a29f039954fcd948c3b8e62dd
Tibor Tóth <toth.tibor@aut.bme.hu>
2020-01-06 21:10:35 +0100

    WIP

--- AdventOfCode2019/AdventOfCode2019.Puzzles/Day19/TractorBeam.cs
+++ AdventOfCode2019/AdventOfCode2019.Puzzles/Day19/TractorBeam.cs
@@ -9,6 +9,7 @@ using AdventOfCode2018.Infrastructure;
 using AdventOfCode2019.Infrastructure;
 using AdventOfCode2019.Puzzles.Extensions;
 using AdventOfCode2019.Puzzles.Intcode;
+using Microsoft.Extensions.Logging;
 
 namespace AdventOfCode2019.Puzzles.Day19
 {
@@ -16,10 +17,12 @@ namespace AdventOfCode2019.Puzzles.Day19
     public class TractorBeam : IPuzzleSolver
     {
         private readonly IIntcodeProgram _intcodeProgram;
+        private readonly ILogger<TractorBeam> _logger;
 
-        public TractorBeam(IIntcodeProgram intcodeProgram)
+        public TractorBeam(IIntcodeProgram intcodeProgram, ILogger<TractorBeam> logger)
         {
             _intcodeProgram = intcodeProgram;
+            _logger = logger;
         }
 
         public async Task<string> SolvePart1Async(Stream input)
@@ -49,9 +52,53 @@ namespace AdventOfCode2019.Puzzles.Day19
             return map.Count(x => x.Value == 1).ToString();
         }
 
-        public Task<string> SolvePart2Async(Stream input)
+        public async Task<string> SolvePart2Async(Stream input)
         {
-            throw new NotImplementedException();
+            var line = await input.ReadLineAsync();
+            var registers = line.Split(',').Select(x => long.Parse(x)).ToArray();
+
+            var map = new Dictionary<(long x, long y), long>();
+            var found = false;
+
+            async Task<long> IsBeamValueAsync(long x, long y)
+            {
+                var outputChannel = Channel.CreateUnbounded<long>();
+                var inputChannel = Channel.CreateUnbounded<long>();
+
+                _intcodeProgram.Init(registers.ToArray()); // copy
+
+                await inputChannel.Writer.WriteAsync(x);
+                await inputChannel.Writer.WriteAsync(y);
+                await _intcodeProgram.RunAsync(inputChannel, outputChannel);
+
+                return await outputChannel.Reader.ReadAsync();
+            }
+
+            for (int i = 10; !found; i++)
+            {
+                Parallel.For(0, i, j =>
+                {
+                    map[(i, j)] = IsBeamValueAsync(i, j).GetAwaiter().GetResult();
+                    map[(j, i)] = IsBeamValueAsync(j, i).GetAwaiter().GetResult();
+                });
+
+                _logger.LogDebug("Finding enough space");
+
+                foreach (var m in map)
+                {
+                    if (m.Value == 1 && map.ContainsKey((m.Key.x + 100, m.Key.y)) && map[(m.Key.x + 100, m.Key.y)] == 1 &&
+                        map.ContainsKey((m.Key.x, m.Key.y + 100)) && map[(m.Key.x, m.Key.y + 100)] == 1)
+                    {
+                        _logger.LogDebug("Enough space found ({x}, {y})", m.Key.x, m.Key.y);
+
+                        return (m.Key.x * 10000 + m.Key.y).ToString();
+                    }
+                }
+
+                _logger.LogDebug("Not found enough");
+            }
+
+            return null;
         }
     }
 }
